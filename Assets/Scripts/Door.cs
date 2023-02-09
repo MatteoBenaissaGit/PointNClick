51c777fc4cc886d3f54c2cbeb9ed6edd9eab5165
MatteoBenaissa <matteobenaissa@gmail.com>
2023-02-09 16:33:55 +0100

    ouais ouais

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using SceneTransition;
@@ -7,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _nextScene;
+    [SerializeField] private Vector3 _playerPlacement;
 
     public void Execute()
     {
@@ -18,6 +20,17 @@ public class Door : MonoBehaviour, IInteractable
     {
         yield return new WaitForSeconds(1f);
         PlayerPrefs.SetString(GameManager.NextSceneKey, _nextScene);
+
+        GameManager.Instance.Player.transform.position = _playerPlacement;
         SceneManager.LoadScene("GameCommon");
     }
+    
+    #if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(_playerPlacement, Vector3.one*0.25f);
+    }
+
+#endif
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+    }
+
     public void Move(Vector2 position)
     {
-        transform.DOKill();
-        transform.DOMove(position, 1f);
+        navMeshAgent.destination = new Vector3(position.x, position.y, transform.position.z);
     }
 }
