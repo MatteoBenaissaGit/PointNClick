a88d229d3aaab9b644e2f8ec557eb6148591c534
MatteoBenaissa <matteobenaissa@gmail.com>
2023-02-15 16:01:02 +0100

    outline

---
+++ Assets/Scripts/OutlineOnHover.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Object = UnityEngine.Object;
+
+public class OutlineOnHover : MonoBehaviour
+{
+    [SerializeField] private Material _baseMaterial;
+    [SerializeField] private Material _outlineMaterial;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().material = _baseMaterial;
+    }
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().material = _outlineMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().material = _baseMaterial;
+
+    }
+
+}
